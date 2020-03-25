be73ce89c32977ea1b06be6a7d52c18f8ae9c020
barath99 <exploretechsbk@gmail.com>
2020-03-25 16:59:47 +0530

    Process: Npm added Node

---
+++ login.js
@@ -0,0 +1,37 @@
+
+////////////////////////////////////////////////////////////////////
+//                                                                //
+//                                                                //
+//                                                                //
+//                                                                //
+//                                                                //
+//                                                                //
+//        YOU SHOULD NOT BE HERE.... BUT ANYWAYS... FOR           //
+//        YOUR CURIOSITY... I GIVE YOU THE ACCESS CODE...         //
+//                                                                //
+//                                                                //
+//                                                                //
+//                                                                //
+//                                                                //
+//                                                                //
+//                                                                //
+//                                                                //
+////////////////////////////////////////////////////////////////////
+
+if(localStorage.getItem('count')==null){
+  localStorage.setItem('count',0);
+}
+
+function validate(){
+  var passcode = document.getElementById('passcode').value;
+  if(passcode=="Kudos#Hacker:P"){
+    window.location.href='home.html';
+  }
+  else{
+  var counter = parseInt(localStorage.getItem('count'));
+  localStorage.setItem('count',counter+1);
+  if(localStorage.getItem('count')>3){
+    alert('Your IP is noted and is blocked for the next 48 hours... Please do not try VPNs. That will be useless...');
+  }
+  }
+}
