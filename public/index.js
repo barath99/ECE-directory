aaca18b7db0bc6029fc75bfea9bedfd419bacaaa
barath99 <exploretechsbk@gmail.com>
2020-04-01 13:18:30 +0530

    Refinement: Structurizing code base

---
+++ public/index.js
@@ -0,0 +1,12 @@
+ const firebaseConfig = {
+    apiKey: "AIzaSyCUhgTOrDeKB4A0eLBEu6udv6u8a2cVdWg",
+    authDomain: "ece-directory.firebaseapp.com",
+    databaseURL: "https://ece-directory.firebaseio.com",
+    projectId: "ece-directory",
+    storageBucket: "ece-directory.appspot.com",
+    messagingSenderId: "613453359986",
+    appId: "1:613453359986:web:25d0899826006422470beb"
+  };
+
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
